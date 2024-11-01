848cc295328d91e9c327c6de271126198139a63e
justsabrin <sabrin16@outlook.dk>
2024-10-31 15:19:34 -1200

    chips til sidesheet

--- scripts/sabrin.js
+++ scripts/sabrin.js
@@ -11,17 +11,20 @@ checkboxes.forEach(checkbox => {
         if (this.checked) {
             // Opret en ny chip
             const chip = document.createElement('div');
-            chip.className = 'chip type-filled';
+            chip.className = 'chip__filled';
             chip.innerHTML = `
-                <div class="type-filled__label">
-                    <div class="type-filled__label-wrapper">
+                  <div class="chip__filled__label">
+                    <div class=".chip__filled__label__wrapper">
                         <div class="type-filled__label2">${this.getAttribute('data-name')}</div>
                     </div>
                 </div>
                 <div class="type-filled__chip-wrapper">
+                    <!-- Tilføjet krydsikon til chippen for at muliggøre fjernelse -->
+                    <svg class="type-filled__cross" focusable="false" aria-hidden="true">
+                        <use xlink:href="#cross"></use>
+                    </svg>
                 </div>
             `;
-            // ÆNDRE CROSS TIL AT DET PASSER IND MED DEN ORGINALE IKON UNDER (<div class="type-filled__chip-wrapper">) ⬆⬆⬆⬆⬆
 
             // Tilføj chippen til chipContainer-elementet
             chipContainer.appendChild(chip);
@@ -40,7 +43,7 @@ checkboxes.forEach(checkbox => {
             const chips = chipContainer.getElementsByClassName('chip');
             for (let i = 0; i < chips.length; i++) {
                 // Hvis chippen matcher checkboxens navn
-                if (chips[i].querySelector('.type-filled__label2').textContent === this.getAttribute('data-name')) {
+                if (chips[i].querySelector('.chip__filled__label2').textContent === this.getAttribute('data-name')) {
                     chipContainer.removeChild(chips[i]);
                     break;
                 }
